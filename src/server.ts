e41f80ac2a8634addff6d65d07b12f14d0b231cb
Guilherme <guilhermezapas@gmail.com>
2024-08-17 11:32:21 -0300

    adding .env and zod schema

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { randomUUID } from "crypto";
+import { env } from "./env";
 
 const app = fastify()
 
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
+    const transactions = await knex('transactions').where('amount', 1000).select('*')
 
     
-    return tables
+
+
+    
+    return transactions
 })
 
 app.listen({
-    port: 3333
+    port: env.PORT
 }).then(() => {
     console.log('HTTP Server Running!')
 })
