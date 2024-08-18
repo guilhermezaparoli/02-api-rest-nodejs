1bf1f144717880b9aaa98e2b7c31350ffb1a4581
Guilherme <guilhermezapas@gmail.com>
2024-08-18 12:50:54 -0300

    adding fastify cookies and preHandlers

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line
 import { Knex } from 'knex';
 
-declare module 'kenx/types/tables' {
-  export interface tables {
+declare module 'knex/types/tables' {
+  export interface Tables {
     transactions: {
       id: string;
       title: string;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,26 +2,102 @@ import { FastifyInstance } from 'fastify';
 import { knex } from '../database';
 import { z } from 'zod';
 import { randomUUID } from 'crypto';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, response) => {
-    const createTransactionBodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
-    });
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
 
-    const { amount, title, type } = createTransactionBodySchema.parse(
-      request.body
-    );
-    // nada abaixo vai executar se houver um erro no parse
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select();
 
-  await knex('transactions').insert({
-      id: randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-    });
-    return response.status(201).send('Transação cadastrada com sucesso!')
-  });
+      return { transactions };
+    }
+  );
 
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionsParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
+
+      const { id } = getTransactionsParamsSchema.parse(request.params);
+      const { sessionId } = request.cookies;
+      const transaction = await knex('transactions')
+        .where({
+          session_id: sessionId,
+          id,
+        })
+        .first();
+
+      return {
+        transaction,
+      };
+    }
+  );
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', {
+          as: 'amount',
+        })
+        .first();
+      return {
+        summary,
+      };
+    }
+  );
+
+  app.post(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, response) => {
+      const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
+      });
+
+      const { amount, title, type } = createTransactionBodySchema.parse(
+        request.body
+      );
+      // nada abaixo vai executar se houver um erro no parse
+
+      let sessionId = request.cookies.sessionId;
+      if (!sessionId) {
+        sessionId = randomUUID();
+
+        response.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        });
+      }
+      await knex('transactions').insert({
+        id: randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+        session_id: sessionId,
+      });
+      return response.status(201).send('Transação cadastrada com sucesso!');
+    }
+  );
 }
