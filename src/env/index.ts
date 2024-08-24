c6ab97c0e5593c8a4ace1aed10f319f46d14a90e
Guilherme <guilhermezapas@gmail.com>
2024-08-24 10:52:19 -0300

    update dependencies

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,7 +13,7 @@ const envSchema = z.object({
   DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
-  
+
 });
 
 const _env = envSchema.safeParse(process.env);
@@ -21,6 +21,7 @@ const _env = envSchema.safeParse(process.env);
 if (_env.success === false) {
   console.error('Invalid enviroment variables!', _env.error.format());
   throw new Error('Invalid enviroment variables');
+  
 }
 
 export const env = _env.data;
